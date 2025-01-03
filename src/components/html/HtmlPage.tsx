4729e9bdc174a655bac20f2b4ef3bb8ce803cb2e
Biki Mandal <bikimandal1832000@gmail.com>
2025-01-03 09:32:06 +0530

    fixed

--- src/components/html/HtmlPage.tsx
+++ src/components/html/HtmlPage.tsx
@@ -30,16 +30,6 @@ const HtmlPage: React.FC = () => {
               Project 3
             </button>
           </Link>
-          <Link to="/html/project4">
-            <button className="bg-teal-500 text-white py-3 px-8 rounded-lg text-xl shadow-md transform hover:scale-105 hover:bg-teal-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-400 active:scale-95">
-              Project 4
-            </button>
-          </Link>
-          <Link to="/html/project5">
-            <button className="bg-teal-500 text-white py-3 px-8 rounded-lg text-xl shadow-md transform hover:scale-105 hover:bg-teal-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-400 active:scale-95">
-              Project 5
-            </button>
-          </Link>
         </div>
 
         {/* Optional content area */}
