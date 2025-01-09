be46505c28627fcb04eb456b5789c75cdaf2a02f
Biki Mandal <bikimandal1832000@gmail.com>
2025-01-09 13:32:13 +0530

    scratcgh-game-2

--- src/components/scratch/scratchprojects/ScratchProjects1.tsx
+++ src/components/scratch/scratchprojects/ScratchProjects1.tsx
@@ -2,15 +2,13 @@ function ScratchProjects1() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-2xl md:text-4xl font-bold text-center mb-6">
-        Scratch Project 1
+        Pigeon - Scratch Game 1
       </h1>
-      {/* <div className="w-full max-w-3xl aspect-w-16 aspect-h-9 relative bg-gray-200 shadow-lg rounded-lg overflow-hidden"> */}
       <iframe
         src="https://scratch.mit.edu/projects/951288241/embed"
         className="absolute top-0 left-0 w-full h-full"
         allowFullScreen
       ></iframe>
-      {/* </div> */}
     </div>
   );
 }
--- src/components/scratch/scratchprojects/ScratchProjects2.tsx
+++ src/components/scratch/scratchprojects/ScratchProjects2.tsx
@@ -1,3 +1,14 @@
 export default function ScratchProjects2() {
-  return <div>scratch project 2</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl md:text-4xl font-bold text-center mb-6">
+        Dino jump game - Scratch Game 2
+      </h1>
+      <iframe
+        src="https://scratch.mit.edu/projects/781668407/embed"
+        className="absolute top-0 left-0 w-full h-full"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
