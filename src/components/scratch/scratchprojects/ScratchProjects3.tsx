5acbd1bc0c446745992852526e5025be891b770a
Biki Mandal <bikimandal1832000@gmail.com>
2025-01-09 13:37:05 +0530

    scratch-game-3

--- src/components/scratch/scratchprojects/ScratchProjects3.tsx
+++ src/components/scratch/scratchprojects/ScratchProjects3.tsx
@@ -1,3 +1,14 @@
 export default function ScratchProjects3() {
-  return <div>scratch project 3</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl md:text-4xl font-bold text-center mb-6">
+        SPACE - Scratch Game 3
+      </h1>
+      <iframe
+        src="https://scratch.mit.edu/projects/468005654/embed"
+        className="absolute top-0 left-0 w-full h-full"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
